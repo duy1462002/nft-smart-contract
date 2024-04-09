244d82351a5994f344735a1d21ed7e5d6cc0c383
VIA Company <duy146123@gmail.com>
2024-04-09 11:06:50 +0700

    smart contract

---
+++ scripts/deploy.js
@@ -0,0 +1,15 @@
+const hre = require("hardhat");
+
+async function main() {
+    const Lock = await hre.ethers.getContractFactory("Lock");
+    const lock = await Lock.deploy();
+
+    await lock.getDeployedCode();
+
+    console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.getAddress}`)
+}
+
+main().catch(error => {
+    console.error(error);
+    process.exitCode = 1;
+})
